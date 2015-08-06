72634095d5d6ed01b9a7c54b05875c86e24704f1
KyleRattet <krattet@gmail.com>
2015-08-06 11:13:29 -0600

    Car class built, adding methods

--- js/main.js
+++ js/main.js
@@ -3,3 +3,22 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+var Car = function (make, model, year, color) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.state = "off";
+  this.color = color;
+  this.previous_owners = [];
+  this.current_owner = [];
+};
+
+var truck = new Car("Toyota", "Tacoma", "1999", "White");
+
+Car.prototype.sale = function(previous_owners, current_owner) {
+  this.current_owner.push(current_owner);
+
+  this.previous_owners.push(previous_owners);
+
+};
