4d584511ef7bc9c8d23c5b004ad3b87a743f8af1
KyleRattet <krattet@gmail.com>
2015-08-06 11:29:38 -0600

    part 1 complete

--- js/main.js
+++ js/main.js
@@ -22,3 +22,32 @@ Car.prototype.sale = function(previous_owners, current_owner) {
   this.previous_owners.push(previous_owners);
 
 };
+
+Car.prototype.paint = function (color) {
+  this.color = color;
+};
+
+Car.prototype.start = function () {
+  this.state = "on";
+};
+
+Car.prototype.off = function () {
+  this.state = "off";
+};
+
+Car.prototype.driveTo = function (destination) {
+  if (this.state === "on") {
+    console.log("Driving to " + destination+".");
+  } else {
+    console.log("Sorry, this car is off.");
+  }
+};
+
+Car.prototype.park = function () {
+  if (this.state === "off") {
+    console.log("Parked.");
+  } else {
+    console.log("Sorry, the car is still on.");
+  }
+};
+
